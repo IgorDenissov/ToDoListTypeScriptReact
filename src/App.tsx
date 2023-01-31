f8f089d16c5b50e0428fa0c7cbc95a92071c90b6
Игорь Денисов <igordenissov83@gmail.com>
2023-01-31 21:08:46 +0300

    added ToDoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Todolist } from './ToDoList';
+import { TaskType } from './ToDoList';
+
+let tasks1:Array <TaskType> = [
+  {id:1, title: "CSS", isDone: true},
+  {id:2, title: "Js", isDone: true},
+  {id:3, title: "React", isDone: false},
+]
+let tasks2 : Array <TaskType> = [
+  {id:1, title: "Terminator", isDone: true},
+  {id:2, title: "XXX", isDone: false},
+  {id:3, title: "StarWars", isDone: false},
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Todolist title = "What to learn" tasks = {tasks1}/>
+     <Todolist title = "Movies" tasks = {tasks2}/>
     </div>
   );
 }
