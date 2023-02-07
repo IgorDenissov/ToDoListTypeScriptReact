4be6767bd8ea3d0ec369ec6875067d40a0aae43b
Игорь Денисов <igordenissov83@gmail.com>
2023-02-07 18:32:25 +0300

    transforming array of objects  into lists of elements

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import './App.css';
 import { Todolist } from './ToDoList';
 import { TaskType } from './ToDoList';
 
-let tasks1:Array <TaskType> = [
+const tasks1:Array <TaskType> = [
   {id:1, title: "CSS", isDone: true},
   {id:2, title: "Js", isDone: true},
   {id:3, title: "React", isDone: false},
 ]
-let tasks2 : Array <TaskType> = [
+const tasks2 : Array <TaskType> = [
   {id:1, title: "Terminator", isDone: true},
   {id:2, title: "XXX", isDone: false},
   {id:3, title: "StarWars", isDone: false},
 ]
+
 
 function App() {
   return (
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 
 export type TaskType = {
-  id:number
+  id: number
   title: string
-  isDone:boolean 
+  isDone: boolean
 }
 
 type PropsType = {
   title: string
-  tasks: Array <TaskType>
+  tasks: Array<TaskType>
 }
 
 export function Todolist(props: PropsType) {
@@ -17,16 +17,23 @@ export function Todolist(props: PropsType) {
     <div>
       <h3>{props.title}</h3>
       <div>
-        <ul>
-          <li><input type="checkbox" checked = {props.tasks[0].isDone} /><span>{props.tasks[0].title}</span></li>
-          <li><input type="checkbox" checked = {props.tasks[1].isDone} /><span>{props.tasks[1].title}</span></li>
-          <li><input type="checkbox" checked = {props.tasks[2].isDone} /><span>{props.tasks[2].title}</span></li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Competed</button>
-        </div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {
+          props.tasks.map((t) => {
+            return <li><input type="checkbox" checked={t.isDone} />
+            <span>{t.title}</span>
+            <button>x</button>
+            </li>
+          })
+        }
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Competed</button>
       </div>
     </div>
   )
